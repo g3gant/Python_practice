6098fd29690dd8b70a3279eec4dd021dd62d4196
G3gant <g3gant@gmail.com>
2022-01-21 14:26:02 -0700

    !

---
+++ ReplaseWords.py
@@ -0,0 +1,14 @@
+
+
+class Replace:
+       
+    def __init__ (self, Start,Old, New):
+        self.Start = Start
+        self.Old = Old
+        self.New = New
+
+rep = List <Replace> {(4,"foo","bar")}
+
+
+
+print (rep)
